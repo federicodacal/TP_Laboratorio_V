d693ce4efc50b3287e2bff1fbb2bd06eaf300741
Federico Dacal <federicodacal@gmail.com>
2023-11-23 06:56:41 -0300

    vista activity libro

--- app/src/main/java/com/example/myapplication/LibroActivity.java
+++ app/src/main/java/com/example/myapplication/LibroActivity.java
@@ -1,9 +1,12 @@
 package com.example.myapplication;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.MenuItem;
 
 import java.io.Serializable;
 
@@ -18,5 +21,17 @@ public class LibroActivity extends AppCompatActivity {
         Serializable ser = bundle.getSerializable("Libro");
         Libro libro = (Libro) ser;
         Log.d("LibroActivity", libro.toString());
+
+        ActionBar ab = super.getSupportActionBar();
+        ab.setTitle(libro.getTitulo());
+        ab.setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId() == android.R.id.home) {
+            super.finish();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
